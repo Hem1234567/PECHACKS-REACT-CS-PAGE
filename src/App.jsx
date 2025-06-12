244d084655f5a39a03120df1530a4e9c252ceb78
Hem1234567 <hem07042006@gmail.com>
2025-06-12 22:06:59 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { BackgroundEffects } from "./components/BackgroundEffects";
 import { TitleSection } from "./components/TitleSection";
 import { CTAButtons } from "./components/CTAButtons";
 import { SocialLinks } from "./components/SocialLinks";
-import { Header } from "./components/Header";
 
 export const App = () => {
   return (
     <BackgroundEffects>
-      <header className="w-full fixed top-0 z-50 bg-transparent backdrop-blur-md sm:hidden">
-        <nav className="max-w-6xl mx-auto flex items-center justify-between px-4 py-3 sm:py-3">
-          <a href="#" className="flex items-center space-x-2 cursor-pointer">
-            <img src="/pechacks bgr.png" alt="Logo" className="h-6 sm:h-8" />
+      <header className="w-full fixed top-0 z-50 bg-transparent backdrop-blur-md">
+        <nav className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3 sm:py-3">
+          <a href="#" className="flex items-center cursor-pointer">
+            <img
+              src="/pechacks bgr.png"
+              alt="Logo"
+              className="h-6 sm:h-8 lg:ml-2 lg:mt-10"
+            />
           </a>
         </nav>
       </header>
 
       <div className="relative z-20 w-full flex flex-col items-center justify-center text-center pt-20 pb-10 px-4 min-h-screen">
         <div className="max-w-4xl w-full mx-auto">
-          <Header/>
           <TitleSection />
           <CTAButtons />
           <SocialLinks />
