a2a92d383e28b20c7c8bc0a44c6c44ec9182c711
Hem1234567 <hem07042006@gmail.com>
2025-06-12 10:28:19 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
-
 import { motion } from "framer-motion";
 
 export const Header = () => {
   return (
     <motion.header
-      className="w-full px-4  hidden sm:flex justify-start"
+      className="w-full hidden sm:flex"
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
     >
-      <img
-        src="/pechacks bgr.png"
-        alt="PEC Hacks Logo"
-        className="h-6 sm:h-8"
-      />
+      <div className="w-full max-w-4xl mx-auto px-4 sm:px-6">
+        <img
+          src="/pechacks bgr.png"
+          alt="PEC Hacks Logo"
+          className="h-6 sm:h-8"
+        />
+      </div>
     </motion.header>
   );
 };
